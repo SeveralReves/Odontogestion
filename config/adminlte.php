46dabd6dbd27bcb74e9cb2233964fd4cbbce015c
Gabo Duran <gduran.ex@meatcode.cl>
2023-10-27 15:17:31 -0400

    change image

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,7 +64,7 @@ return [
     */
 
     'logo' => '<b>Odonto</b>Gestion',
-    'logo_img' => 'resources/images/logo.png',
+    'logo_img' => 'vendor/adminlte/dist/img/logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
